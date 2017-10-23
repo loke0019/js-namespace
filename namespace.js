8d40060b5f198b6d0ad0269634a1d381c53d651f
lokisgr <32423425+lokisgr@users.noreply.github.com>
2017-10-23 01:09:18 -0400

    final

--- namespace.js
+++ namespace.js
@@ -1,22 +1,26 @@
 var LOKE0019 = {
     init: function () {
+        console.log("script in LOKE0019 called");
         var div = document.createElement("div");
+
         div.className = "box";
         div.textContent = "LOKE0019";
+
         document.getElementById("boxes").appendChild(div);
+
         div.addEventListener("click", click_me);
         div.addEventListener("mouseout", hoverMouse);
         div.addEventListener("mouseover", hoverMouse);
 
 
-        function click_me(ev) {
-            this.style.backgroundColor = "green";
-            this.style.borderColor = "blue";
-        }
-
         function hoverMouse(ev) {
-            ev.currentTarget.classList.toggle("highlight");
+            this.classList.toggle("highlight");
         }
 
+        function click_me(ev) {
+            this.style.backgroundColor = "pink";
+            this.style.borderColor = "green";
+        }
     }
+
 }
